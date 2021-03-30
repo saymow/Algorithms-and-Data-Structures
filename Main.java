c1fe1318bafab2abc605f50b04fa257989424fcc
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-03-29 21:02:45 -0300

    feat: add menu

--- Main.java
+++ Main.java
@@ -4,22 +4,48 @@ class Main {
   static Scanner console = new Scanner(System.in);
 
   public static void main(String[] args) {
-    SinglyLinkedList singlyLinkedList = new SingleLinkedList();
-    boolean finished;
+    SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+    int option;
 
+    System.out.println("------------Singly Linked List------------");
+    System.out.println("Options:");
     do {
-      System.out.print("Type a number: ");
-      singlyLinkedList.push(Integer.parseInt(console.nextLine()));
 
-      System.out.print("Do you wanna add more numbers (Y/N):");
-      finished = console.nextLine().toLowerCase().charAt(0) == 'n';
-    } while (!finished);
+      System.out.println("1 - Push a number");
+      System.out.println("2 - Pop a number");
+      System.out.println("3 - Check if includes a number");
+      System.out.println("4 - show all numbers");
+      System.out.println("5 - exit\n");
+      System.out.print("Chose your option: ");
+      option = Integer.parseInt(console.nextLine());
+      
+      System.out.print("\n\n");
+      
+      switch (option) {
+        case 1: {
+          System.out.print("Type a number: ");
+          singlyLinkedList.push(Integer.parseInt(console.nextLine()));
+          break;
+        }
+        case 2: {
+          System.out.println("Element popped.");
+          singlyLinkedList.pop();
+          break;
+        }
+        case 3: {
+          System.out.print("Type a number: ");
+          System.out.println((singlyLinkedList.includes(Integer.parseInt(console.nextLine())) ? "yep" : "no"));
+          break;
+        }
+        case 4: {
+          System.out.println("Current elements.");
+          singlyLinkedList.list();
+          break;      
+        }
+      }
 
-    singlyLinkedList.pop();
-    singlyLinkedList.list();
+      System.out.print("\n\n\n");
 
-    System.out.println(
-      "Does it includes 3? " + (singlyLinkedList.includes(3) ? "yep" : "no")
-    );
+    } while (option != 5);
   }
 }
