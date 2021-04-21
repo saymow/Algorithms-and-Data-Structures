0f766f7a73d6c36d01c299a6b2722ebfd0a53093
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-04-21 17:36:55 -0300

    feat(algorithms): add mergeSort

--- algorithms/sorting/algorithms.js
+++ algorithms/sorting/algorithms.js
@@ -1,1 +1,2 @@
 export { default as bubbleSort } from "./bubbleSort.js";
+export { default as mergeSort } from "./mergeSort.js";
---
+++ algorithms/sorting/mergeSort.js
@@ -0,0 +1,23 @@
+export default function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+  const arr = [];
+
+  while (left.length !== 0 && right.length !== 0) {
+    if (left[0] <= right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  return [...arr, ...left, ...right];
+};
