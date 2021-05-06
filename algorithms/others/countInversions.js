8c54c4c724fde23dcb5360d5333ce8033ccc54a3
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-05-06 20:40:39 -0300

    feat(algorithms): add inversions count

---
+++ algorithms/others/countInversions.js
@@ -0,0 +1,41 @@
+const _countInversion = (args) => {
+  let [arr, count] = args;
+  if (arr.length === 1) return args;
+
+  const mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  let [sortedLeft, x] = _countInversion([left, count]);
+  let [sortedRight, y] = _countInversion([right, count]);
+  let [sortedArr, z] = countSplitInversion(sortedLeft, sortedRight);
+
+  return [sortedArr, x + y + z];
+};
+
+const countSplitInversion = (left, right) => {
+  const arr = [];
+  let invCount = 0,
+    i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] > right[j]) {
+      arr.push(right[j++]);
+      invCount += left.length - i;
+    } else {
+      arr.push(left[i++]);
+    }
+  }
+
+  return [[...arr, ...left.slice(i), ...right.slice(j)], invCount];
+};
+
+const countInversion = (arr) => {
+  //complexity (n.logn)
+  return _countInversion([arr, 0])[1];
+};
+
+console.log(countInversion([1, 6, 4, 3, 7]) === 3);
+console.log(countInversion([8, 4, 2, 1]) === 6);
+console.log(countInversion([3, 5, 1, 4, 3]) === 5);
