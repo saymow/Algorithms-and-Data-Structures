3c8bcf7f2195943b2b228987a55d3dd7ac7c1a6a
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2021-05-20 22:32:06 -0300

    feat(algorithms): add HeapSort

--- algorithms/sorting/algorithms.js
+++ algorithms/sorting/algorithms.js
@@ -1,2 +1,3 @@
 export { default as bubbleSort } from "./bubbleSort.js";
 export { default as mergeSort } from "./mergeSort.js";
+export { default as heapSort } from "./mergeSort.js";
---
+++ algorithms/sorting/heapSort.js
@@ -0,0 +1,41 @@
+const swap = (arr, i, j) => {
+  let tmp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = tmp;
+};
+
+const heapify = (arr, i, n) => {
+  let largestIndex = i;
+  const leftChildIndex = 2 * i + 1;
+  const rightChildIndex = 2 * i + 2;
+
+  if (leftChildIndex < n && arr[leftChildIndex] > arr[largestIndex]) {
+    largestIndex = leftChildIndex;
+  }
+
+  if (rightChildIndex < n && arr[rightChildIndex] > arr[largestIndex]) {
+    largestIndex = rightChildIndex;
+  }
+
+  if (largestIndex !== i) {
+    swap(arr, i, largestIndex);
+
+    heapify(arr, i, largestIndex);
+  }
+};
+
+export default function heapSort(arr) {
+  const n = arr.length;
+
+  for (let i = Math.floor(n / 2 - 1); i >= 0; i--) {
+    heapify(arr, i, n);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    swap(arr, 0, i);
+
+    heapify(arr, 0, i);
+  }
+
+  return arr;
+}
