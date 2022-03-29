b58f304fe5180d7592b55a6d476adf300b827ef4
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-03-28 23:04:35 -0300

    feat(algorithms): add nodeDepths algorithm

---
+++ algorithms/others/nodeDepths.js
@@ -0,0 +1,19 @@
+// This is the class of the input binary tree.
+class BinaryTree {
+  value;
+  left;
+  right;
+
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// O(n) time | O(h) space - where h is height of the tree
+export function nodeDepths(root, depth = 0) {
+  if (root === null) return 0;
+	
+  return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+}
